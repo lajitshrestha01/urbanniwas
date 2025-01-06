3173aa761ab3521c7acecc95218476bf71044029
lajitshrestha01 <lajitshrestha96@gmail.com>
2025-01-06 21:07:45 +0545

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js'
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -21,3 +22,4 @@ mongoose.connect(process.env.MONGO)
 
 
  app.use('/api/user', userRouter);
+ app.use('/api/auth', authRouter);
