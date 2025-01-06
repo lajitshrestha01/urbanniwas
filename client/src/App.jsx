f81ee83adb701d691ee259712ee2a1a7a6208e46
lajitshrestha01 <np03cs4a220225@heraldcollege.edu.np>
2025-01-06 20:27:03 +0545

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import SignIn from './pages/SignIn';
+import Home from './pages/Home';
+import SignOut from './pages/SignOut';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header'
+
+
+export default function App() {
+  return (
+  <BrowserRouter>
+  <Header/>
+  <Routes>
+
+   <Route path="/" element = {<Home />}  />
+   <Route path="/sign-in" element = {<SignIn />}  />
+   <Route path="/sign-out" element = {<SignOut />}  />
+   <Route path="/about" element = {<About />}  />
+   <Route path="/profile" element = {<Profile />}  />
+  </Routes>
+  
+  </BrowserRouter>
+  );
+  
+}
