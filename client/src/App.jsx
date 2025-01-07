ced27da5759107a6729fa8b1e638f4ab6deaa382
lajitshrestha01 <lajitshrestha96@gmail.com>
2025-01-07 19:18:19 +0545

    signup page done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import {BrowserRouter, Routes, Route } from 'react-router-dom'
-import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import Home from './pages/Home';
 import SignOut from './pages/SignOut';
 import About from './pages/About';
@@ -14,7 +14,7 @@ export default function App() {
   <Routes>
 
    <Route path="/" element = {<Home />}  />
-   <Route path="/sign-in" element = {<SignIn />}  />
+   <Route path="/sign-up" element = {<SignUp />}  />
    <Route path="/sign-out" element = {<SignOut />}  />
    <Route path="/about" element = {<About />}  />
    <Route path="/profile" element = {<Profile />}  />
