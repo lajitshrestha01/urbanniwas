3e769cf196df700ea1c80e1aecc5295abbbcc809
lajitshrestha01 <lajitshrestha96@gmail.com>
2025-01-07 13:27:55 +0545

    created middleware and error handler for auth

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,17 +1,19 @@
 import User from "../model/user.model.js";
-import bcryptjs from 'bcryptjs';
-
-export const signup = async (req, res) =>{
-    const {username, email, password} = req.body;
-    const hashedPassword = bcryptjs.hashSync(password,10);
-    const newUser = new User({username, email, hashedPassword});
+import bcryptjs from 'bcryptjs'
+export const signup = async (req, res, next) => {
+    const { username, email, password} = req.body;
+    const hashedpassword = bcryptjs.hashSync(password,10)
+    const newUser = new User({username,email,password: hashedpassword});
     try{
-        await newUser.save()
-        res.status(201).json("user created sucessfully");
+        await newUser.save();
+        res.status(201).json("User created successfully");
     }
     catch(error){
-        res.status(500).json(error.message);
-
+        next(error);
     }
+
+
+
+
 
 };
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -4,3 +4,4 @@ import { signup } from '../controllers/auth.controller.js';
 const router = express.Router();
 
 router.post("/signup",signup);
+export default router;
